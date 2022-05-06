d305089816f4873f2858d9bdb092923e1c04533f
Pierre Gaimard <pierre@gaimard-family.fr>
2022-05-06 14:11:21 +0200

    Add init project packages and config

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./templates/**/*.html.twig", "./assets/vue/**/*.vue"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      red: colors.red,
+      grey: colors.grey,
+      cyan: colors.cyan,
+      orange: colors.orange,
+      green: colors.green,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
