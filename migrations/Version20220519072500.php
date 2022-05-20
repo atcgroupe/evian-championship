8f4cd1ed0502d911be4fd464a5e1f18051d01d34
Pierre Gaimard <pierre@gaimard-family.fr>
2022-05-20 12:20:45 +0200

    Add new database migrations

---
+++ migrations/Version20220519072500.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220519072500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user_event_notification (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, event SMALLINT NOT NULL, INDEX IDX_FE039AC2A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user_event_notification ADD CONSTRAINT FK_FE039AC2A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user_event_notification');
+    }
+}
