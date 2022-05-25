5f9f5aa5ba44b3b2b2af835e19c096b8478da709
Pierre Gaimard <pierre@gaimard-family.fr>
2022-05-25 14:48:02 +0200

    Add fixtures to UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Enum\UserRole;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,19 +12,27 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $customer = new User();
-        $customer->setFirstName('Customer');
-        $customer->setLastName('Customer');
-        $customer->setEmail('informatique@atc-groupe.com');
-        $customer->setPassword('$2y$13$sljKu8z5iZDq0NPeSDiUt.hh0rkUe563OvSPgDvlTqOn2LDPLq/AK');
-        $customer->setRoles([User::ROLE_CUSTOMER]);
+        $customer->setFirstName('Justine');
+        $customer->setLastName('Buisson');
+        $customer->setEmail('justine.buisson@danone.com');
+        $customer->setPassword('$2y$13$3gOUJVfrIQK9a4QC/WBXFuD/7DPWCQoz29nnByRyqd6ln6jgMm6C6 ');
+        $customer->setRoles([UserRole::CUSTOMER->getValue()]);
         $manager->persist($customer);
+
+        $customer2 = new User();
+        $customer2->setFirstName('Vincent');
+        $customer2->setLastName('Ripoll');
+        $customer2->setEmail('vincent@vincentripoll.fr');
+        $customer2->setPassword('$2y$13$R0CU4R4WmBZEBkt1/6nGq.KA16WX5vN0QBZBLu2NUHEiWxhCLjJFC');
+        $customer2->setRoles([UserRole::CUSTOMER->getValue()]);
+        $manager->persist($customer2);
 
         $projectManager = new User();
         $projectManager->setFirstName('Anaïs');
         $projectManager->setLastName('Duparc');
         $projectManager->setEmail('anais.duparc@atc-groupe.com');
         $projectManager->setPassword('$2y$13$xX/fp/WatQyTcxe9M7wy2.dCCBe920lsA.QV4U5Man.TnNfaQvxzq');
-        $projectManager->setRoles([User::ROLE_PROJECT_MANAGER]);
+        $projectManager->setRoles([UserRole::PROJECT_MANAGER->getValue()]);
         $manager->persist($projectManager);
 
         $graphicDesigner = new User();
@@ -31,7 +40,7 @@ class UserFixtures extends Fixture
         $graphicDesigner->setLastName('Poëzévara');
         $graphicDesigner->setEmail('laura.poezevara@atc-groupe.com');
         $graphicDesigner->setPassword('$2y$13$NeG7gqamiQ3kjY79GUGczeo4CqVo0e/fnCNKeC5lYZDgnERzGOtzm');
-        $graphicDesigner->setRoles([User::ROLE_GRAPHIC_DESIGNER]);
+        $graphicDesigner->setRoles([UserRole::GRAPHIC_DESIGNER->getValue()]);
         $manager->persist($graphicDesigner);
 
         $shippingManager = new User();
@@ -39,7 +48,7 @@ class UserFixtures extends Fixture
         $shippingManager->setLastName('Florio');
         $shippingManager->setEmail('nathalie.florio@atc-groupe.com');
         $shippingManager->setPassword('$2y$13$sseN.BFWBNFfcOrqdom.jOudUieokuPQC2SCHsgv3Xkiy1fYGK0zm');
-        $shippingManager->setRoles([User::ROLE_SHIPPING_MANAGER]);
+        $shippingManager->setRoles([UserRole::SHIPPING_MANAGER->getValue()]);
         $manager->persist($shippingManager);
 
         $admin = new User();
@@ -47,7 +56,7 @@ class UserFixtures extends Fixture
         $admin->setLastName('Gaimard');
         $admin->setEmail('pierre.gaimard@atc-groupe.com');
         $admin->setPassword('$2y$13$SxBNPPDnoiAC4LDn6iC1U.0myw.qzh4aeyBz8JkSJoDzUMVgZI5xO');
-        $admin->setRoles([User::ROLE_ADMIN]);
+        $admin->setRoles([UserRole::ADMIN->getValue()]);
         $manager->persist($admin);
 
         $manager->flush();
